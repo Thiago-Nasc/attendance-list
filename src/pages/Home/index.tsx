468f08a62977e1a916ec2044ab08136c75ff890d
Thiago <thiagonascimentodeveloper@gmail.com>
2022-07-13 16:27:58 -0300

    terminado a inserção de items de forma interativa

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,17 +1,37 @@
 import './style.css';
 import { Card } from '../../components/Card';
+import React, { useState } from 'react';
 
 export function Home() {
+
+  type NewPerson = any[];
+
+  const [personPresent, setPersonPresent] = useState('');
+  const [personList, setPersonList] = useState<NewPerson>([]);
+
+  function handleAddStudent() {
+    let newPerson = {
+      nome: personPresent,
+      time: new Date().toLocaleTimeString('pt-br', {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit'
+      })
+    };
+
+    setPersonList( prevState => [...prevState, newPerson]);
+  };
 
   return (
     <div className="Home">
 
       <div className="areaInput">
-        <h1>Lista de despesas</h1>
-        <input type="text"/>
-        <button>adicionar</button>
+        <h1>Lista de Presença</h1>
+        <input type="text" onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPersonPresent(e.target.value)}/>
+        <button onClick={handleAddStudent}>adicionar</button>
       </div>
-      <div className="cards">
+      <div id="cards">
+        { personList?.map( (person, i) => <Card name={person.nome} time={person.time} key={i}/>)}
       </div>
     </div>
   )
